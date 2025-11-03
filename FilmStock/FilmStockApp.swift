b35b30ecabd48905f44b6d1f1d6b0fdc01036a2d
Jonas Halbe <jonas.halbe@cognite.com>
2025-11-03 16:29:18 +0100

    Initial Commit

---
+++ FilmStock/FilmStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmStockApp.swift
+//  FilmStock
+//
+//  Created by Jonas  Halbe on 03/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FilmStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
